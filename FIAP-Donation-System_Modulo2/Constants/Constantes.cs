68eab7ffd2b70e59bab5c4815eaa0cad8696e13f
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-17 18:43:12 -0300

    Constants created

---
+++ FIAP-Donation-System_Modulo2/Constants/Constantes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Constants
+{
+    public static class Constantes
+    {
+    }
+}
---
+++ FIAP-Donation-System_Modulo2/Constants/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Constants
+{
+    public static class Mensagem
+    {
+        public static readonly string MS_001 = "Cadastro realizado com sucesso!";
+        public static readonly string MS_002 = "Digite um número válido";
+    }
+}
