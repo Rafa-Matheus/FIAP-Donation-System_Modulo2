f42587815bcfc8700b4e60e94b78288b1b4c6f69
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-17 18:42:59 -0300

    Enums created

---
+++ FIAP-Donation-System_Modulo2/Enums/ETamanhoRoupa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Enums
+{
+    public enum ETamanhoRoupa
+    {
+        P,
+        M,
+        G,
+        GG,
+        [Description("2")]
+        _2,
+        [Description("4")]
+        _4,
+        _6,
+        _8,
+        _10,
+        _12,
+        _14,
+        _16
+    }
+}
---
+++ FIAP-Donation-System_Modulo2/Enums/ETipoAjuste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Enums
+{
+    public enum ETipoAjuste
+    {
+        Cadarco = 0,
+        Velcro = 1,
+        Botao = 2,
+        Cacefacil = 3,
+        Ziper = 4
+    }
+}
