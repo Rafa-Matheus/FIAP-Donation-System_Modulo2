b5178b487ec333f69069f9ccc2c43608899576de
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-17 18:40:14 -0300

    Create IAutenticavel interface

---
+++ FIAP-Donation-System_Modulo2/Interfaces/IAutenticavel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Interfaces
+{
+    public interface IAutenticavel
+    {
+
+    }
+}
