f426ce0241bcf6df84d174bfb0d9f6e3eb2e5470
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-17 18:41:22 -0300

    Models created

---
+++ FIAP-Donation-System_Modulo2/Models/Calcado.cs
@@ -0,0 +1,23 @@
+﻿using FIAP_Donation_System_Modulo2.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Models
+{
+    public class Calcado : Produto
+    {
+
+        public string Material { get; set; }
+        public ETamanhoCalcado Tamanho { get; set; }
+        public ECategoriaCalcado Categoria { get; set; }
+        public ETipoAjuste TipoAjuste { get; set; }
+
+        public override string ExibirDados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FIAP-Donation-System_Modulo2/Models/Roupa.cs
@@ -0,0 +1,28 @@
+﻿using FIAP_Donation_System_Modulo2.Enums;
+using FIAP_Donation_System_Modulo2.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Donation_System_Modulo2.Models
+{
+    public class Roupa : Produto
+    {
+
+        public ECategoriaRoupa Categoria { get; set; }
+        public ETamanhoRoupa Tamanho { get; set; }
+        public string Material { get; set; }
+        public EComprimentoRoupa TipoComprimento { get; set; }
+
+        public override string ExibirDados()
+        {
+            StringBuilder sb = new StringBuilder();
+            //sb.Append(base.ExibirDados());
+            sb.Append($"\nCategoria: {Categoria} \nTamanho: {Tamanho.GetDescription()}\nComprimento Roupa: {TipoComprimento.GetDescription()}");
+
+            return sb.ToString();
+        }
+    }
+}
